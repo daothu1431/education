9bf587f9bbb847591bb340c2032e890e729d931b
DaoThuPro <Thu86065@st.vimaru.edu.vn>
2023-10-23 22:45:31 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,22 @@
-
 // =============== Active Navbar =============================
-
 // Lấy danh sách tất cả các liên kết trong menu
 const navbarLinks = document.querySelectorAll(".navbar__link");
 
 // Lặp qua từng liên kết và thêm sự kiện click
 navbarLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
-        // Loại bỏ lớp navbar-active khỏi tất cả các liên kết
-        navbarLinks.forEach((link) => {
-            link.classList.remove("navbar-active");
-        });
+        // Kiểm tra nếu liên kết đã có lớp navbar-active, thì không thay đổi gì cả
+        if (!this.classList.contains("navbar-active")) {
+            // Loại bỏ lớp navbar-active khỏi tất cả các liên kết
+            navbarLinks.forEach((link) => {
+                link.classList.remove("navbar-active");
+            });
 
-        // Gắn lớp navbar-active vào liên kết được click
-        this.classList.add("navbar-active");
-
-        // Ngăn sự kiện mặc định của liên kết (nếu có)
-        event.preventDefault();
+            // Gắn lớp navbar-active vào liên kết được click
+            this.classList.add("navbar-active");
+        } else {
+            // Nếu liên kết đã có lớp navbar-active, thì mặc định của nó sẽ hoạt động
+            return;
+        }
     });
 });
-
-
-// ==========
